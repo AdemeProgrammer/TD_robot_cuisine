633f0e6a6540541e2411d136897bd8992bb10a8a
Ademe <A.AYAD@lprs.fr>
2025-01-09 14:52:32 +0100

    [feat] Avancement projet + ajout word
    Le word a été rajouté. De plus, le TD a été complété jusqu'à la phase 4.
    Il ne reste plus que la phase finale et la phase complémenatire.

--- src/Ingrédient.java
+++ src/Ingrédient.java
@@ -1,5 +1,24 @@
 public class Ingrédient {
-    public static void main(String[] args) {
-        
-    }
+        private String nom;
+        private int quantite;
+        private String unite;
+
+        public void getNom(){
+            System.out.println("Nom: " + nom);
+        }
+        public void getQuantite(){
+            System.out.println("Quantite: " + quantite);
+        }
+        public void getUnite(){
+            System.out.println("Unite: " + unite);
+        }
+        public void setQuantite(int quantite){
+            this.quantite = quantite;
+        }
+
+        public Ingrédient(String nom,int quantite,String unite){
+            this.nom = nom;
+            this.quantite = quantite;
+            this.unite = unite;
+        }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Saisissez le nom de l'ingrédient");
+        System.out.println("Saisissez la quantité de l'ingrédient");
+        System.out.println("Saisissez l'unité de l'ingrédient");
 
     }
 }
