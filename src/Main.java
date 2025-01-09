79bcb68ebe7775cd0ba3c64e92c6b70c052dc8a8
Ademe <A.AYAD@lprs.fr>
2025-01-09 15:30:25 +0100

    [feat] Entrer nom, quantité, unité

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-        System.out.println("Saisissez le nom de l'ingrédient");
-        System.out.println("Saisissez la quantité de l'ingrédient");
-        System.out.println("Saisissez l'unité de l'ingrédient");
+        System.out.println("Que voulez vous faire : ");
+        System.out.println("1 - Voir les ingrédients");
+        System.out.println("2 - Ajouter un ingrédient");
+        int choix = clavier.nextInt();
+        if (choix == 2) {
+            System.out.println("Saisissez le nom de l'ingrédient");
+            String nomIngredient = clavier.next();
+            System.out.println("Saisissez la quantité de l'ingrédient");
+            int quantiteIngredient = clavier.nextInt();
+            System.out.println("Saisissez l'unité de l'ingrédient");
+            String uniteIngredient = clavier.next();
+            new Ingrédient(nomIngredient, quantiteIngredient, uniteIngredient);
+            Ingrédient monIngrédient = new Ingrédient(nomIngredient, quantiteIngredient, uniteIngredient);
+        }
+        else if (choix == 1) {
+            System.out.println("Voici les ingrédients :");
+        }
 
     }
 }
